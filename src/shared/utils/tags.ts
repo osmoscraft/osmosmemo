6e6f6f2f20a1eef32f7e72bda4749a8bad815731
Chuanqi Sun <chuanqi-no-reply@gmail.com>
2021-04-14 20:49:17 -0700

    remove deadcode

--- src/shared/utils/tags.ts
+++ src/shared/utils/tags.ts
@@ -4,7 +4,6 @@ import { browser } from 'webextension-polyfill-ts';
 
 /** Parse a markdown string for all words prefixed by hashtag(#). Ignore words that are inside parentheses and brackets */
 export async function getUniqueTagsFromMarkdownString(markdownString) {
-  const data: Options = await browser.storage.sync.get(['accessToken', 'username', 'repo', 'filename']);
   // negative look ahead to rule out any hashtags inside parenthesis
   const hashTags = markdownString.match(/(?!.*(?:\)|]))#([a-z0-9]+)/g) || [];
   const textTags = hashTags.map((tag) => tag.split('#')[1]);
