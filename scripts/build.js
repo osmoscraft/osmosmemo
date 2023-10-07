b838a51b80935a6b780ab54e8ffc788d70507ef3
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-10-07 13:23:31 -0700

    remove fs-extra

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const fs = require("fs-extra");
+const fs = require("fs/promises");
 
 const OUT_DIR = `dist`;
 const UNPACKED_OUT_DIR = `dist/unpacked`;
@@ -10,7 +10,7 @@ async function build() {
   console.log("[build] start building extension...");
 
   console.log(`[build] remove ${OUT_DIR}`);
-  fs.removeSync(path.resolve(OUT_DIR));
+  await fs.rm(path.resolve(OUT_DIR), { recursive: true, force: true });
 
   await require("esbuild")
     .build({
@@ -34,11 +34,11 @@ async function build() {
     .catch(() => process.exit(1));
 
   const copyTasksAsync = [
-    fs.copyFile(path.resolve("src/options.html"), path.join(UNPACKED_OUT_DIR, "options.html")),
-    fs.copyFile(path.resolve("src/popup.html"), path.join(UNPACKED_OUT_DIR, "popup.html")),
-    fs.copyFile(path.resolve("src/manifest.json"), path.join(UNPACKED_OUT_DIR, "manifest.json")),
-    fs.copyFile(path.resolve("src/styles.css"), path.join(UNPACKED_OUT_DIR, "styles.css")),
-    fs.copy(path.resolve("src/assets"), path.join(UNPACKED_OUT_DIR, "assets")),
+    fs.cp(path.resolve("src/options.html"), path.join(UNPACKED_OUT_DIR, "options.html")),
+    fs.cp(path.resolve("src/popup.html"), path.join(UNPACKED_OUT_DIR, "popup.html")),
+    fs.cp(path.resolve("src/manifest.json"), path.join(UNPACKED_OUT_DIR, "manifest.json")),
+    fs.cp(path.resolve("src/styles.css"), path.join(UNPACKED_OUT_DIR, "styles.css")),
+    fs.cp(path.resolve("src/assets"), path.join(UNPACKED_OUT_DIR, "assets"), { recursive: true }),
   ];
 
   await Promise.all(copyTasksAsync);
