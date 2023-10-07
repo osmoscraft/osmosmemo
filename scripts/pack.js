1b7474e728550a901a9ca473ee477e2a1efd550d
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-10-07 14:06:46 -0700

    migrate packing script

--- scripts/pack.js
+++ scripts/pack.js
@@ -1,5 +1,6 @@
 import assert from "assert/strict";
 import { exec } from "child_process";
+import { readFile } from "fs/promises";
 import path from "path";
 import { promisify } from "util";
 import { readJson } from "./fs.js";
@@ -16,7 +17,11 @@ async function pack(dir) {
   console.log("[pack] extension dir", path.resolve(dir));
   const manifest = await readJson(path.resolve(dir, "manifest.json"));
   const version = manifest.version;
-  const outFilename = `fjord-${version}.chrome.zip`;
+  const packageName = await readFile("package.json", "utf-8")
+    .then(JSON.parse)
+    .then((pkg) => pkg.name);
+
+  const outFilename = `${packageName}-${version}.chrome.zip`;
 
   await execAsync(`zip -r ../${outFilename} .`, { cwd: dir });
 
