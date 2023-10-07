8ddfdbffbd27925acd1757bb7202110c0a639ae4
chuanqisun <chuanqi.sun.dev@gmail.com>
2023-10-07 14:15:26 -0700

    add missing json reader

--- scripts/pack.js
+++ scripts/pack.js
@@ -3,7 +3,6 @@ import { exec } from "child_process";
 import { readFile } from "fs/promises";
 import path from "path";
 import { promisify } from "util";
-import { readJson } from "./fs.js";
 
 const execAsync = promisify(exec);
 
@@ -17,15 +16,21 @@ async function pack(dir) {
   console.log("[pack] extension dir", path.resolve(dir));
   const manifest = await readJson(path.resolve(dir, "manifest.json"));
   const version = manifest.version;
-  const packageName = await readFile("package.json", "utf-8")
-    .then(JSON.parse)
-    .then((pkg) => pkg.name);
+  const packageName = await readJson("package.json").then((pkg) => pkg.name);
 
   const outFilename = `${packageName}-${version}.chrome.zip`;
 
   await execAsync(`zip -r ../${outFilename} .`, { cwd: dir });
 
   console.log(`[pack] packed: ${outFilename}`);
+}
+
+/**
+ *
+ * @param {string} path
+ */
+async function readJson(path) {
+  return JSON.parse(await readFile(path, "utf-8"));
 }
 
 pack(dir);
